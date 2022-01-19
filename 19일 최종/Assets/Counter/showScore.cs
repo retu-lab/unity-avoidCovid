2f4ca6e9c951a67661b76d366f10fcf8d1bba4ee
swrho901 <91191844+swrho901@users.noreply.github.com>
2022-01-19 22:09:40 +0900

    final19

---
+++ 19일 최종/Assets/Counter/showScore.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showScore : MonoBehaviour
+{
+    public int lastCount;
+    [SerializeField] Text Score;
+    void Start()
+    {
+        lastCount = Forever_ShowCount_score.lastScore;
+        Score.text = lastCount.ToString();
+    }
+
+}
