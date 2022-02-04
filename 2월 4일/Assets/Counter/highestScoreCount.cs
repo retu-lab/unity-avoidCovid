b59531028fb6c9695482bdbb0a3f6f4d258db5c6
swrho901 <91191844+swrho901@users.noreply.github.com>
2022-02-04 12:11:28 +0900

    d

---
+++ 2월 4일/Assets/Counter/highestScoreCount.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class highestScoreCount : MonoBehaviour
+{
+    [SerializeField] Text HS;
+    public static int Score = Forever_ShowCount_score.lastScore;
+    // public Text scoreText = showScore.Score;
+   // public Text highScoreText;
+
+    private int savedScore = 0;
+    private string KeyString = "HighScore";
+
+    void Awake()
+    {
+        savedScore = PlayerPrefs.GetInt(KeyString, 0);
+        //highScoreText.text = "High Score : " + savedScore.ToString();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // scoreText.text = "Score : " + Score.ToString("0");
+
+        if (Score > savedScore)
+        {
+            PlayerPrefs.SetInt(KeyString, Score);
+        }
+        HS.text = "High Score : " + savedScore.ToString();
+    }
+}
